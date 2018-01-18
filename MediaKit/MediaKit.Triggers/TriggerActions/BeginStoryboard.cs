eaef777e20b7356a0ff3b4b4670ce103c2d6050c
VMosiyuk <vasyl.mosiiuk@globallogic.ocm>
2018-01-18 14:23:34 +0800

    - added XML-comments for MediaKit.Triggers project

--- MediaKit/MediaKit.Triggers/TriggerActions/BeginStoryboard.cs
+++ MediaKit/MediaKit.Triggers/TriggerActions/BeginStoryboard.cs
@@ -4,9 +4,21 @@ using Xamarin.Forms;
 
 namespace MediaKit.Triggers.TriggerActions
 {
-    public class BeginStoryboard : TriggerAction<VisualElement>
+    /// <summary>
+    ///     Trigger action, activates specified <see cref="Storyboard" /> with trigger's owner as an animation root.
+    /// </summary>
+    public sealed class BeginStoryboard : TriggerAction<VisualElement>
     {
+        /// <summary>
+        ///     <see cref="MediaKit.Core.Storyboard" /> object, which is trigger's action target.
+        /// </summary>
         public Storyboard Storyboard { get; set; }
+
+        /// <summary>
+        ///     Application developers override this method to provide the action that is performed when the trigger condition
+        ///     is met.
+        /// </summary>
+        /// <param name="target">The object on which to invoke the trigger action.</param>
         protected override void Invoke(VisualElement target)
         {
             Storyboard?.Begin(target);
